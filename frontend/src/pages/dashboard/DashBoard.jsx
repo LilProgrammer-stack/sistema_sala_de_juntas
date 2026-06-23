3811f367e3e93b2819ff5476051ff6241ddacccb
Miguel Flores <floresjosex50@gmail.com>
2026-06-22 19:25:38 -0600

    Create project structure

---
+++ frontend/src/pages/dashboard/DashBoard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+    return <h1>My Requests</h1>;
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/new_reservation/NewReservation.jsx
@@ -0,0 +1,5 @@
+function NewReservation() {
+    return <h1>New Reservation</h1>;
+}
+
+export default NewReservation;
